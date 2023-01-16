8631a99850a2348fae395313889a891644f11804
Eda <edaalbayrak86@gmail.com>
2023-01-16 19:08:25 +0300

    testNG

--- src/test/java/tests/day16_SmokeTests/C01_PozitifLoginTest.java
+++ src/test/java/tests/day16_SmokeTests/C01_PozitifLoginTest.java
@@ -13,6 +13,12 @@ public class C01_PozitifLoginTest {
     public void pozitifLoginTest(){
 
         // qualitydemy anasayfaya git
+
+        //gitmek istediğimiz sayfanın url'ini girmemiz gerekiyor.
+        //manuel olarak htpps çeklinde girmemek için bunu dinamik yapmalıyız
+        //test datalarımızı configration.proporties file'ına yükledik
+        //bu file java dışında olduğu için buna direk ulaşamayız, configReader üzerinden ulaşabilriz
+
         Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
 
         QualitydemyPage qualitydemyPage= new QualitydemyPage();
@@ -27,5 +33,9 @@ public class C01_PozitifLoginTest {
 
         Assert.assertTrue(qualitydemyPage.basariliGirisCoursesLinki.isDisplayed());
         Driver.closeDriver();
+
+        //bu class'taki her şey dinamik
+        //değişen bir şey olursa diğerlerine gider ve orada güncelleriz
+        //bu şekilde tüm bilgiler güncellenmiş olur
     }
 }
